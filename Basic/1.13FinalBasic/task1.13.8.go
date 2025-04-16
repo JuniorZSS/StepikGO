f97db7e4433bf14d6fec97aaaf83f894270857f1
Запевалов Сергей Сергеевич <sergejj.s.zapevalov@vitech.team>
2025-04-16 19:57:00 +0300

    Решаю задачу 8 из базовой части

--- Basic/1.13FinalBasic/task1.13.8.go
+++ Basic/1.13FinalBasic/task1.13.8.go
@@ -1,24 +1,30 @@
 package __13FinalBasic
 
+import "fmt"
+
 func Task8() {
-	//fmt.Println("Создайте массив и найдите количество минимальных значений!")
-	//var a int
-	//fmt.Print("Введите количество элементов в массиве: ")
-	//fmt.Scan(&a)
-	//
-	//arr := make([]int, a)
-	//fmt.Print("Введите количество числа в массив: ")
-	//
-	//for i := range arr {
-	//	fmt.Scan(&arr[i])
-	//}
-	//j := 0
-	//for i := range arr {
-	//	arr2 := arr[i]
-	//	if arr[i] == arr2 && arr[i] >= arr2 {
-	//		j++
-	//	}
-	//
-	//}
-	//fmt.Print(j)
+	fmt.Println("Создайте массив и найдите количество минимальных значений!")
+	var a int
+	fmt.Print("Введите количество элементов в массиве: ")
+	fmt.Scan(&a)
+
+	arr := make([]int, a)
+	fmt.Print("Введите количество числа в массив: ")
+
+	for i := range arr {
+		fmt.Scan(&arr[i])
+	}
+	m := arr[0]
+	for i := range arr {
+		if m > arr[i] {
+			m = arr[i]
+		}
+	}
+	j := 0
+	for i := range arr {
+		if m == arr[i] {
+			j++
+		}
+	}
+	fmt.Print(j)
 }
