43799044d18911447ef3238122a9002d6b0a44d5
Запевалов Сергей Сергеевич <sergejj.s.zapevalov@vitech.team>
2025-05-10 17:59:00 +0300

    15Task

--- Basic/1.13FinalBasic/task1.13.15.go
+++ Basic/1.13FinalBasic/task1.13.15.go
@@ -1,5 +1,24 @@
 package __13FinalBasic
 
+import "fmt"
+
 func Task15() {
-	//TASK15
+
+	fmt.Print("Введите число и цифру, которую нужно убрать из вашего числа. Ваше число и цифра: ")
+
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	res := 0
+	arr := []int{}
+	for a > 0 {
+		res = a % 10
+		if res != b {
+			arr = append(arr, res)
+		}
+		a = a / 10
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+		fmt.Print(arr[i])
+	}
 }
